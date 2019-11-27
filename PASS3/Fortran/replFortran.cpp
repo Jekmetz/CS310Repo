3d2fd61bd77106692073baa7b341c385305182d0
ashly718 <ashly718@gmail.com>
2019-11-27 10:25:10 -0500

    fortranStuff

---
+++ PASS3/Fortran/replFortran.cpp
@@ -0,0 +1,21 @@
+/*
+ * replFortran.cpp
+ *
+ *  Created on: Nov 26, 2019
+ *      Author: ashlylovings
+ */
+
+#include "replFortran.h"
+
+replFortran::replFortran() {
+	//TODO constructor stub
+
+}
+
+
+replFortran::~replFortran() {
+	// TODO Auto-generated destructor stub
+}
+
+
+
---
+++ PASS3/Fortran/replFortran.h
@@ -0,0 +1,21 @@
+/*
+ * replFortran.h
+ *
+ *  Created on: Nov 26, 2019
+ *      Author: ashlylovings
+ */
+
+#ifndef REPLFORTRAN_H_
+#define REPLFORTRAN_H_
+
+class replFortran {
+public:
+	replFortran();
+	virtual ~replFortran();
+
+};
+
+
+
+#endif /* REPLFORTRAN_H_ */
+
